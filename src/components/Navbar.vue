7f79f01254cc41189fade1aca7ef17d72e7008cb
lolvOid <linhtet1998itachi@gmail.com>
2023-05-24 20:12:34 +0100

    UPDATED NAVBAR

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,13 @@
 <template>
   <v-app-bar color="white" elevation="0">
+    
     <template v-slot:prepend>
-      <v-img src="icon.png" class="pa-5 mr-2" />
-      <v-img src="logo_text.png" class="pa-10" />
+      <v-btn v-if="$route.name==='edit.resume'" variant="text" icon="mdi:mdi-arrow-left" @click="$router.back()"></v-btn>
+      <v-btn class="pa-0 h-auto" variant="text" :to="{name:'home'}" color="white">
+        <v-img src="icon.png" class="pa-5 mr-2" />
+        <v-img src="logo_text.png" class="pa-10" />
+      </v-btn>
+      
     </template>
     <!-- 
     <v-app-bar-title>
